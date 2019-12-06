77b172c16e90a598b8641cd81aef201e5df89ac6
Kitae Lee <vivasoul@naver.com>
2019-12-07 02:34:13 +0900

    chore: Prepare to develope main page..

--- src/main/java/com/griptk/b2b/product/service/ProductController.java
+++ src/main/java/com/griptk/b2b/product/service/ProductController.java
@@ -1,8 +1,15 @@
 package com.griptk.b2b.product.service;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ProductController {
-
+public class ProductController {	
+	@RequestMapping("/main")
+	public String goMainPage(Model model) {
+		model.addAttribute("content_page", "product/list");
+		
+		return "_template/main";
+	}
 }
--- src/main/webapp/WEB-INF/jsp/product/list.jsp
+++ src/main/webapp/WEB-INF/jsp/product/list.jsp
@@ -1,12 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="EUC-KR">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
-</html>
+<div style="display:block;min-height:720px;">
+	쇼핑몰 상품 리스트입니다~
+</div>	
