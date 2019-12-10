add9b41a1ecb4a369aeb6dd74651fc24300c0143
MSI_2 <MSI_2@DESKTOP-T2OLKTE>
2019-12-10 14:56:34 +0900

    update. info_change initial commit

--- src/main/java/com/griptk/b2b/user/service/UserAPI.java
+++ src/main/java/com/griptk/b2b/user/service/UserAPI.java
@@ -29,6 +29,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.griptk.b2b.user.domain.ImageVO;
 import com.griptk.b2b.user.domain.UserVO;
 import com.griptk.b2b.user.mapper.UserMapper;
+import com.griptk.b2b.util.generator.MailSendUtil;
+import com.griptk.b2b.util.generator.PasswordGenerator;
 
 @RestController
 @MapperScan({"com.griptk.b2b.user.mapper.*"})
@@ -36,6 +38,12 @@ public class UserAPI {
 	
 	@Autowired
 	private UserMapper mapper;
+	
+	@Autowired
+	private MailSendUtil mailSender;
+	
+	@Autowired 
+	private PasswordGenerator passwordGenerator;
 	
 	// id_check
 	@PostMapping(path="/sign_up/check/id", produces="application/json")
@@ -234,24 +242,6 @@ public class UserAPI {
 		return resp;
 	}
 	
-//	@Value("${mail.host}")
-//	private String host;
-//	
-//	@Value("${mail.port}")
-//	private String port;
-//	
-//	@Value("${mail.username}")
-//	private String username;
-//	
-//	@Value("${mail.password}")
-//	private String password;
-//	
-//	@Value("${mail.protocol}")
-//	private String protocol;
-//	
-//	@Value("${mail.tls}")
-//	private boolean tls;
-	
 	public int mailSender(String recipient){
 		String username="yohan394";
 	    String password="nM3947242!!";
@@ -284,7 +274,6 @@ public class UserAPI {
             message.setSubject("Testing Subject");
             message.setText("Dear Mail Crawler," 
             + "\n\n No spam to my email, please!");//내용 
- //message.setContent("내용","text/html; charset=utf-8");//글내용을 html타입 charset설정
             System.out.println("send!!!");
             Transport.send(message); 
             System.out.println("SEND");
@@ -296,59 +285,13 @@ public class UserAPI {
         return 1;
 	}
 	
-	
-	public int mailSender2(String recipient) { 
-		try{
-			
-			
-			// 네이버일 경우 smtp.naver.com 을 입력합니다. 
-			// Google일 경우 smtp.gmail.com 을 입력합니다.
-			String host = "smtp.gmail.com"; 
-			final String username = "zzangnyh123"; 
-			//네이버 아이디를 입력해주세요. @nave.com은 입력하지 마시구요. 
-			final String password = "nM3947242!@"; 
-			//네이버 이메일 비밀번호를 입력해주세요. 
-			int port=465; 
-			//포트번호 // 메일 내용 
-			recipient = "yohan394@gmail.com"; 
-			//받는 사람의 메일주소를 입력해주세요. 
-			String subject = "메일테스트"; //메일 제목 입력해주세요. 
-			String body = username+"님으로 부터 메일을 받았습니다."; 
-			//메일 내용 입력해주세요.
-			Properties props = System.getProperties();
-			// 정보를 담기 위한 객체 생성 // SMTP 서버 정보 설정 
-			props.put("mail.smtp.host", host); 
-			props.put("mail.smtp.port", port); 
-			props.put("mail.smtp.auth", "true"); 
-			props.put("mail.smtp.ssl.enable", "true"); 
-			props.put("mail.smtp.ssl.trust", host); 
-			//Session 생성 
-			Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-				String un=username; 
-				String pw=password; 
-				protected javax.mail.PasswordAuthentication getPasswordAuthentication() { 
-					return new javax.mail.PasswordAuthentication(un, pw);
-				} 
-			}); 
-			session.setDebug(true); 
-			//for debug 
-			Message mimeMessage = new MimeMessage(session); 
-			//MimeMessage 생성 
-			mimeMessage.setFrom(new InternetAddress("XXXXXXXX@naver.com")); 
-			//발신자 셋팅 , 보내는 사람의 이메일주소를 한번 더 입력합니다. 이때는 이메일 풀 주소를 다 작성해주세요. 
-			mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
-			//수신자셋팅 //.TO 외에 .CC(참조) .BCC(숨은참조) 도 있음
-			mimeMessage.setSubject(subject);
-			//제목셋팅 
-			mimeMessage.setText(body); 
-			//내용셋팅 
-			Transport.send(mimeMessage); 
-			//javax.mail.Transport.send() 이용 }
-			return 1;
-		}catch(Exception e){
-			return 0;
-		}
-		
+	private void sendTempPassword(String email, String password) throws Exception{
+		String title = "Sending a temporary passowrd to access the solution, PPCWIZ";
+		StringBuilder sb = new StringBuilder();
+		  sb.append("This is your temporary password:<br/><input type='text' value='")
+		  	.append(password)
+		  	.append("'/>");
+		mailSender.send(email, title, sb.toString());
 	}
 
 	
---
+++ src/main/webapp/WEB-INF/jsp/user/info_change.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<script src="./lib/js/jquery.min.js"></script>
+<script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="./lib/css/withdrawal.css">
+<link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+ <div class="custom-container container">    
+		<div class="mainbox col-sm-10 col-sm-offset-2">
+		<br>
+      	<br>
+      	<br>
+	
+		<table style="border-collapse: collapse;width:100%">
+			<tbody style="width: 100%;display: table;">
+			<tr>
+				<td>
+				</td>
+			</tr>	
+			<tr>
+				<td>
+				</td>
+			</tr>	
+			</tbody>
+		</table>
+      	<br>
+      	<br>
+      	<br>
+      	<div class="row">
+      		<div class="col-sm-12 submit" align="center">
+      			 <button id="sign_up" class="btn-primary btn-lg" type="submit">확인</button>
+      			 <button class="btn-warning btn-lg" type="button">취소</button>
+      		</div>
+         </div>
+         
+        </div>
+
+</div>
+</body>
+</html>
