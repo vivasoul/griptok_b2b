e741c03e86181d2b46dbbe3e5d555c02ad48d6fe
DHYH_DH <dhyj777@gmail.com>
2019-12-24 10:27:01 +0900

    fix : sample mapping of tb_menu url to qna detail page

--- src/main/java/com/griptk/b2b/qna/service/QnaController.java
+++ src/main/java/com/griptk/b2b/qna/service/QnaController.java
@@ -1,11 +1,10 @@
 package com.griptk.b2b.qna.service;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.griptk.b2b.qna.domain.QnaDetailVO;
 import com.griptk.b2b.qna.domain.QnaVO;
@@ -18,22 +17,22 @@ public class QnaController {
 		return "qna/list";
 	}
 	
-	@RequestMapping("/qna/{qnaId}")
-	public ModelAndView detail(@PathVariable String qnaId,ModelMap model) {
+	@RequestMapping("/qna/view")
+	public String detail(Model model,@RequestParam("id") String qnaId) {
 		String viewPath = "qna/detail";
-		ModelAndView mav =  new ModelAndView(viewPath);
+		model.addAttribute("content_page", viewPath);
 		
 		//[TO-DO]query detail data and setAttribute in mav. Create temporary data in detail.jsp for now.
 		QnaVO qnaVo = new QnaVO(12,"이건 제목입니다.","이러쿵저러쿵에 대해서 질문해도 되죠?","2019-12-04","답변 완료");
-		mav.addObject("detailObject",qnaVo);
+		model.addAttribute("detailObject",qnaVo);
 		
 		//[TO-DO]query answer data and setAttribute in mav. NO DATABASE AVAILABLE ATM FOR ANSWERS.
 //		QnaDetailVO qdv = new QnaDetailVO(12, null);
 		QnaDetailVO qdv = new QnaDetailVO(12, "이러쿵저러쿵에 대한 관리자의 답변입니다.");
 		
-		mav.addObject("answerObject",qdv);
+		model.addAttribute("answerObject",qdv);
 		
-		return mav;
+		return "_template/main";
 	}
 	
 	@GetMapping("/qna/write")
