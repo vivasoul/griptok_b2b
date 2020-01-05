da3f93e120d5acdc2b7f3aa0d2e541c030a5af21
Kitae Lee <vivasoul@naver.com>
2020-01-05 23:43:59 +0900

    feat: Integrated with Cart/interestedAPI

--- src/main/resources/static/js/product/detail.js
+++ src/main/resources/static/js/product/detail.js
@@ -43,4 +43,38 @@ const loadData = function(product_id) {
 		}).always(function(){
 			
 		});
-}
+};
+
+const moveToCart = function(product_id, carted_cnt) {
+	jQuery.ajax({
+		  url: "/carteds",
+		  method: "POST",
+		  contentType: "application/json",
+		  data: JSON.stringify([
+			  {"product_id":product_id, "carted_cnt": carted_cnt}
+		  ])
+		}).done(function(e){
+			alert("해당 상품을 장바구니에 추가하였습니다.");
+		}).fail(function(e){
+			console.log(e);
+		}).always(function(){
+			
+		});	
+};
+
+const moveToInterested = function(product_id) {
+	jQuery.ajax({
+		  url: "/interesteds",
+		  method: "POST",
+		  contentType: "application/json",
+		  data: JSON.stringify([
+			  {"product_id":product_id}
+		  ])
+		}).done(function(e){
+			alert("해당 상품을 관심상품에 추가하였습니다.");
+		}).fail(function(e){
+			console.log(e);
+		}).always(function(){
+			
+		});	
+};
